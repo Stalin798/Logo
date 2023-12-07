60dd8ce18c4f3c4d47a44f26d4485c42698400f2
lovelystalin391@gmail.com <lovelystalin391@gmail.com>
2023-12-07 21:59:02 +0530

    login

---
+++ Sravan.java
@@ -0,0 +1,37 @@
+class Sravan{
+  public static void main(String args[]){
+    System.out.print("Mom is waiting for your success");
+    System.out.print("Be Serious Brooo...");
+    System.out.print("Life is not about girls");
+    System.out.print("You have dreams to chase");
+    System.out.print("Focus on goals Not a girls");
+    System.out.print("Don't chase Girls..");
+    System.put.print("Follow Your Golas...");
+    System.out.print("Automatically Girls chase you");
+    System.out.print("You want to won this world");
+    System.out.print("May be not today...");
+    System.out.print("May be not tommorow...");
+    System.out.print("But");
+    System.out.print("One day You will become...");
+    System.out.print("the Champion of this world...");
+    System.out.print("your parents are never...");
+    System.out.print("Pressure me in studies...");
+    System.out.print("their silent expressions are...");
+    System.out.print("killing you...");
+    System.out.print("See your MOM and DAD...");
+    System.out.print("They are become old...");
+    System.out.print("So be fast bro...");
+    System.out.print("They are waiting for...");
+    System.out.print("your success...");
+    System.out.print("Don't compare your self...");
+    System.out.print("To othes...");
+    System.out.print("There is no diffrence between");
+    System.out.print("the SUN and the MOON...");
+    System.out.print("they Shine when their it's time");
+    System.out.print("No one is important...");
+    System.out.print("In front of your career...");
+    System.out.print("Your parents worked Hard to..");
+    System.out.print("See you successfull...");
+    System.out.print("Not to see you cry for a girl..");
+  }
+}
